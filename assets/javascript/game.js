381d5212c5386cf68319a09901ab55adb12a2a2f
Kevin Davis <dawnpatrol506@gmail.com>
2018-08-25 21:19:49 -0700

    added questions array

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+for(i = 0; i < 10; i++){
+    console.log(questions[i]);
+}
+
+});
---
+++ assets/javascript/questions.js
@@ -0,0 +1,39 @@
+function Question(question, f1, f2, f3, t){
+    this.question = question;
+    this.f1 = f1;
+    this.f2 = f2;
+    this.f3 = f3;
+    this.t = t;
+}
+
+var questions = [];
+
+var q1 = new Question('Come As You Are, a song by the grunge band Nirvana, was released on which album?', '', '', '', 'Nevermind');
+questions.push(q1);
+
+var q2 = new Question('When referring to a type of music, what does R&B stand for?', 'Rock and Bowl', 'Rivets and Bolts', 'Rocky and Bullwinkle', 'Rhythm and Blues');
+questions.push(q2);
+
+var q3 = new Question('Who was the first woman to be inducted into the Rock and Roll Hall of Fame?', 'Diana Ross', 'Carly Simon', 'Carol King', 'Aretha Franklin');
+questions.push(q3);
+
+var q4 = new Question('Who was the first performer at the 1969 Woodstock festival', 'Jimmy Hendrix', 'The Who', 'Ten Years After', 'Richie Havens');
+questions.push(q4);
+
+var q5 = new Question('Houses of the Holy is fifth studio album by which English rock band?', 'The Rolling Stones', 'Dire Straits', 'The Guess Who', 'Led Zeppelin');
+questions.push(q5);
+
+var q6 = new Question('What American music duo released the studio album "Sounds of Silence" in 1966', 'Captain and Tenille', 'Sonny and Cher', 'Bert and Ernie', 'Simon and Garfunkel');
+questions.push(q6);
+
+var q7 = new Question('Who interrupted Taylor Swifts acceptance speech at the 2009 Video Music Awards?', 'Kim Kardashian', 'Wilson Philips', 'Flavor Flav', 'Kanye West');
+questions.push(q7);
+
+var q8 = new Question('What is the name of the Grateful Deads debut album released in 1967?', 'Hell Freezes Over', 'Seconds to Midnight', 'Big Ones', 'The Grateful Dead');
+questions.push(q8);
+
+var q9 = new Question('What is the first album released by American rock band Bon Jovi?', 'Living on a Prayer', 'Dead or Alive', 'Unplugged', 'Bon Jovi');
+questions.push(q9);
+
+var q10 = new Question('What is the stage name of the member of Public Enemy who would later have a reality dating show?', 'Snoop Dog', 'The Bachelor', 'Lil Wayne', 'Flavor Flav');
+questions.push(q10);
