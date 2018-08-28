b4ddb3a8c1b314755a24b4e35fe5f4e4553eec5e
Kevin Davis <dawnpatrol506@gmail.com>
2018-08-27 19:51:01 -0700

    script now generates questions and answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
 
-for(i = 0; i < 10; i++){
-    console.log(questions[i]);
+function startGame(){
+    for(i = 0; i < 10; i++){
+        //console.log('first loop');
+        var mainRow = $("<div class='row full-question'></div>");
+        
+        var q = $("<h5 class='question-text'>" + questions[i].question + "</h5>");
+        //console.log(q);
+        mainRow.append(q);
+
+        var radioRow = $("<div class='radio-row'></div>");
+        mainRow.append(radioRow);
+
+        for(j = 0; j < 4; j++){
+            var a = $("<input type='radio' name='answer" + i + "'></input>");
+
+            var lbl = $("<label for='answer" + j + "'>" + questions[i].answers[j] + "</label>");
+
+            radioRow.append(a);
+            radioRow.append(lbl);
+        }
+
+        //console.log(row);
+        $(".game-area").append(mainRow);
+    }
+    var submitBtn = $("<button id='sbmt'>Submit</button>");
+    $(".game-area").append(submitBtn);
+
 }
+
+$("#start-game").on('click', function(){
+    $("#start-game").hide();
+    startGame();
+})
 
 });
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,14 +1,11 @@
 function Question(question, f1, f2, f3, t){
     this.question = question;
-    this.f1 = f1;
-    this.f2 = f2;
-    this.f3 = f3;
-    this.t = t;
+    this.answers = [f1, f2, f3, t];
 }
 
 var questions = [];
 
-var q1 = new Question('Come As You Are, a song by the grunge band Nirvana, was released on which album?', '', '', '', 'Nevermind');
+var q1 = new Question('Come As You Are, a song by the grunge band Nirvana, was released on which album?', 'Unplugged', 'Bleach', 'In Utero', 'Nevermind');
 questions.push(q1);
 
 var q2 = new Question('When referring to a type of music, what does R&B stand for?', 'Rock and Bowl', 'Rivets and Bolts', 'Rocky and Bullwinkle', 'Rhythm and Blues');
