35bb4bbbab92a78611e5b701ac91a2b464030629
Kevin Davis <dawnpatrol506@gmail.com>
2018-08-27 21:18:04 -0700

    game logic working, needs timer and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,89 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-function startGame(){
-    for(i = 0; i < 10; i++){
-        //console.log('first loop');
-        var mainRow = $("<div class='row full-question'></div>");
-        
-        var q = $("<h5 class='question-text'>" + questions[i].question + "</h5>");
-        //console.log(q);
-        mainRow.append(q);
+    var outOfTen = 0;
 
-        var radioRow = $("<div class='radio-row'></div>");
-        mainRow.append(radioRow);
+    function startGame() {
+        for (i = 0; i < 10; i++) {
+            var mainRow = $("<div class='row full-question'></div>");
 
-        for(j = 0; j < 4; j++){
-            var a = $("<input type='radio' name='answer" + i + "'></input>");
+            var q = $("<h5 class='col-12 question-text'>" + questions[i].question + "</h5>");
+            //console.log(q);
+            mainRow.append(q);
 
-            var lbl = $("<label for='answer" + j + "'>" + questions[i].answers[j] + "</label>");
+            var radioRow = $("<div class='radio-row" + i + "'></div>");
+            mainRow.append(radioRow);
 
-            radioRow.append(a);
-            radioRow.append(lbl);
+            var randoArray = randomizeArray(questions[i].answers);
+
+            for (j = 0; j < 4; j++) {
+                var a = $("<input type='radio' name='answer" + i + "' value='" + randoArray[j] + "'></input>");
+
+                var lbl = $("<label for='answer" + j + "'>" + randoArray[j] + "</label>");
+
+                radioRow.append(a);
+                radioRow.append(lbl);
+            }
+
+            //console.log(row);
+            $(".game-area").append(mainRow);
+        }
+        var submitBtn = $("<button class='col-12' id='sbmt'>Submit</button>");
+        $(".game-area").append(submitBtn);
+
+    }
+
+    function randomizeArray(array) {
+        var arr = [];
+
+        for(var i = 0; i < array.length; i++){
+            arr.push(array[i]);
         }
 
-        //console.log(row);
-        $(".game-area").append(mainRow);
+        for (var i = 0; i < arr.length; i++) {
+            var rand = Math.floor(Math.random() * arr.length);
+
+            var swapper = arr[i];
+            arr[i] = arr[rand];
+            arr[rand] = swapper;
+        }
+
+        return arr;
     }
-    var submitBtn = $("<button id='sbmt'>Submit</button>");
-    $(".game-area").append(submitBtn);
 
-}
+    function checkAnswers() {
+        for (var i = 0; i < questions.length; i++) {
+            var selected = $(".radio-row" + i).children(":checked");
+            if (selected.val() === questions[i].answers[3])
+                outOfTen++;
+        }
+    }
 
-$("#start-game").on('click', function(){
-    $("#start-game").hide();
-    startGame();
-})
+    function displayResults(){
+        $(".game-area").empty();
+        
+        var result = $("<h1 class='col-12'>Your Score: " + outOfTen + "/10</h1>");
+        var playAgain = $("<div class='row'><button class='btn-primary col-12' id='play-again'>Play Again?</button></div>");
+
+        $(".game-area").append(result, playAgain);
+    }
+
+    function restartGame(){
+        $(".game-area").empty();
+        var startButton = $("<button class='col-12 btn-primary' id='start-game'>Start</button>");
+        $(".game-area").append(startButton);
+        outOfTen = 0;
+    }
+
+    $(document).on('click', '#sbmt', function(){
+        checkAnswers();
+        displayResults();
+    });
+
+    $(document).on('click', '#start-game', function () {
+        $("#start-game").hide();
+        startGame();
+    });
+
+    $(document).on('click', '#play-again', restartGame);
 
 });
