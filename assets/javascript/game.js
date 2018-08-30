560844cb3565c7e3f43577e21bbecad19c4ba182
Kevin Davis <dawnpatrol506@gmail.com>
2018-08-29 18:46:11 -0700

    styled, clicking label now clicks radio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var interval;
 
     function startGame() {
-        var timer = $("<h1 id='timer'>90</h3>")
+        var timer = $("<h1 id='timer' class='col-12 text-center'>90</h3>")
         $(".game-area").append(timer);
 
         var timeRemaining = 90;
@@ -25,13 +25,13 @@ $(document).ready(function () {
         }, 1000);
 
         for (i = 0; i < 10; i++) {
-            var mainRow = $("<div class='row full-question'></div>");
+            var mainRow = $("<div class='text-center row full-question'></div>");
 
             var q = $("<h5 class='col-12 question-text'>" + qArray[i].question + "</h5>");
             //console.log(q);
             mainRow.append(q);
 
-            var radioRow = $("<div class='radio-row" + i + "'></div>");
+            var radioRow = $("<div class='text-center col-12 radio-row" + i + "'></div>");
             mainRow.append(radioRow);
 
             var randoArray = randomizeArray(qArray[i].answers);
@@ -82,15 +82,17 @@ $(document).ready(function () {
     function displayResults() {
         $(".game-area").empty();
 
-        var result = $("<h1 class='col-12'>Your Score: " + outOfTen + "/10</h1>");
-        var playAgain = $("<div class='row'><button class='btn-primary col-12' id='play-again'>Play Again?</button></div>");
+        var result = $("<h1 id='score' class='col-12 text-center'>Your Score: " + outOfTen + "/10</h1>");
+        var playAgain = $("<button class='btn-danger col-12' id='play-again'>Play Again?</button>");
 
-        $(".game-area").append(result, playAgain);
+        $(".bg").append(result);
+        $(".bg").append(playAgain);
     }
 
     function restartGame() {
-        $(".game-area").empty();
-        var startButton = $("<button class='col-12 btn-primary' id='start-game'>Start</button>");
+        $("#play-again").remove();
+        $("#score").remove();
+        var startButton = $("<button class='col-12 btn-danger' id='start-game'>Start</button>");
         $(".game-area").append(startButton);
         outOfTen = 0;
     }
@@ -108,5 +110,10 @@ $(document).ready(function () {
     });
 
     $(document).on('click', '#play-again', restartGame);
+
+    $(document).on('click', 'label', function(){
+        var radio = $(this).prev();
+        radio.prop('checked', true);
+    });
 
 });
